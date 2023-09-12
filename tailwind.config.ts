7b3bdb31b824998103aeaddcf63b161c3f660396
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-09-12 12:31:40 +0800

    feat(tailwind): configure default font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,16 @@ const config: Config = {
       xl: "1280px",
       "2xl": "1536px",
     },
+    fontSize: {
+      'body2': '0.8rem',
+      'body1': '1rem',
+      'h6': '1rem',
+      'h5': '1.25rem',
+      'h4': '1.563rem',
+      'h3': '1.953rem',
+      'h2': '2.441rem',
+      'h1': '3.052rem'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
