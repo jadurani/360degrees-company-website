d351c1f06a6c7dd01326bbdc5525f5ebc8e9f633
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-09-18 10:57:49 +0800

    feat: team section

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -7,6 +7,8 @@ import { Fact } from "@components/Fact/Fact";
 import quoteIcon from "@assets/icons/quote.svg";
 import { CORE_VALUES, MILESTONE_LIST } from "./contants";
 import { MilestoneItem } from "@components/MilestoneItem/MilestoneItem";
+import { Person } from "@components/Person/Person";
+import teamPhoto from "@assets/images/about-us-team-1.png"
 
 export default function AboutUs() {
   return (
@@ -100,7 +102,55 @@ export default function AboutUs() {
       </section>
 
       {/* commitment */}
+
       {/* our team */}
+      <section className="py-14 px-8 w-screen bg-accent-50">
+        <div className="container mx-auto max-w-6xl text-neutral-800">
+          <h1 className="font-header text-h2 text-center font-bold">
+            Our Team
+          </h1>
+
+          <p className="font-body my-4 text-center">
+            Some description about the founders and the team...
+          </p>
+
+          <div className="flex flex-wrap gap-7 justify-center mt-10">
+            <Person
+              name="Rafaela Santos"
+              title="Co-Founder"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nunc tortor, viverra ut aliquam id, aliquam vitae enim."
+              photoSrc="/images/unsplash_92z4a5V7y4I.png"
+              website="https://www.360degreessystemscorporation.com/"
+              instagram="http://instagram.com/"
+              linkedin="https://linkedin.com/"
+              email="info@360degrees.com.ph"
+            />
+            <Person
+              name="Isabella Reyes"
+              title="Co-Founder"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nunc tortor, viverra ut aliquam id, aliquam vitae enim."
+              photoSrc="/images/aiony-haust-3TLl_97HNJo-unsplash.jpg"
+              website="https://www.360degreessystemscorporation.com/"
+              instagram="http://instagram.com/"
+              linkedin="https://linkedin.com/"
+              email="info@360degrees.com.ph"
+            />
+          </div>
+
+          <div className="flex flex-col-reverse lg:flex-col gap-4 max-w-2xl mx-auto mt-8">
+            <div className="relative min-w-xs w-full h-full">
+              <Image src={teamPhoto} alt="360degrees team" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              quis dui posuere, euismod lorem sed, pharetra felis. Nam eleifend
+              vulputate felis et gravida. Morbi sed purus eget mauris congue
+              suscipit id at lectus.
+            </p>
+          </div>
+        </div>
+      </section>
+
       {/* our milestones */}
       <section className="py-14 px-8 w-screen bg-gradient-to-b from-accent-50 to-neutral-0">
         <div className="container mx-auto max-w-6xl text-neutral-800">
@@ -122,7 +172,12 @@ export default function AboutUs() {
           <div className="relative my-8 py-4 text-neutral-800">
             {MILESTONE_LIST.map((m, idx) => (
               <div key={idx} className="my-8">
-                <MilestoneItem direction={m.direction} year={m.year} mainEvent={m.mainEvent} otherEvents={m.otherEvents ?? []} />
+                <MilestoneItem
+                  direction={m.direction}
+                  year={m.year}
+                  mainEvent={m.mainEvent}
+                  otherEvents={m.otherEvents ?? []}
+                />
               </div>
             ))}
 
@@ -131,7 +186,9 @@ export default function AboutUs() {
             <div className="absolute z-0 h-3 -bottom-3 left-1/2 -translate-x-1/2 border-dotted border-l-2 border-neutral-800"></div>
           </div>
 
-          <div className="-mt-3 text-h6 font-header font-bold text-center">Onwards to more exciting projects! </div>
+          <div className="-mt-3 text-h6 font-header font-bold text-center">
+            Onwards to more exciting projects!{" "}
+          </div>
         </div>
       </section>
 
--- src/components/Person/Person.stories.tsx
+++ src/components/Person/Person.stories.tsx
@@ -3,8 +3,8 @@ import { Person } from "./Person";
 
 const meta = {
   title: "Person",
-  component: Person
-} satisfies Meta<typeof Person>
+  component: Person,
+} satisfies Meta<typeof Person>;
 
 export default meta;
 
@@ -12,13 +12,28 @@ type Story = StoryObj<typeof meta>;
 
 export const RafaelaSantos: Story = {
   args: {
-    name: 'Rafaela Santos',
-    title: 'Co-Founder',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nunc tortor, viverra ut aliquam id, aliquam vitae enim.',
-    photoSrc: '/images/unsplash_92z4a5V7y4I.png',
-    website: 'https://www.360degreessystemscorporation.com/',
-    instagram: 'http://instagram.com/',
-    linkedin: 'https://linkedin.com/',
-    email: 'info@360degrees.com.ph'
-  }
+    name: "Rafaela Santos",
+    title: "Co-Founder",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nunc tortor, viverra ut aliquam id, aliquam vitae enim.",
+    photoSrc: "/images/unsplash_92z4a5V7y4I.png",
+    website: "https://www.360degreessystemscorporation.com/",
+    instagram: "http://instagram.com/",
+    linkedin: "https://linkedin.com/",
+    email: "info@360degrees.com.ph",
+  },
+};
+
+export const IsabellaReyes: Story = {
+  args: {
+    name: "Isabella Reyes",
+    title: "Co-Founder",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nunc tortor, viverra ut aliquam id, aliquam vitae enim.",
+    photoSrc: "/images/aiony-haust-3TLl_97HNJo-unsplash.jpg",
+    website: "https://www.360degreessystemscorporation.com/",
+    instagram: "http://instagram.com/",
+    linkedin: "https://linkedin.com/",
+    email: "info@360degrees.com.ph",
+  },
 };
