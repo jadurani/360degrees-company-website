51d0a6a2d20cca2096d2ad29da71498d9a72a86b
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-10-04 14:20:15 +1100

    fix(projects-page): prevent scrolling when modal is open

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,7 +1,25 @@
-export default async function Layout(props: {
+"use client";
+import {
+  StoryViewerProps,
+} from "@components/StoryViewer/StoryViewer";
+import { useSearchParams } from "next/navigation";
+import { getProjectFromSlug } from "./completed-projects.constant";
+
+export default function Layout(props: {
   allProjects: React.ReactNode,
   projectView: React.ReactNode
 }) {
+  const searchParams = useSearchParams();
+  const projectViewSlug = searchParams.get("projectView") ?? "";
+  const projectDetails: StoryViewerProps | undefined =
+    getProjectFromSlug(projectViewSlug);
+
+  if (projectDetails) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
+
   return (
     <>
       {props.projectView}
