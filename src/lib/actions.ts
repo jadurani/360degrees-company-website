5efecb23c8662339a1241808eef1c6d4c2a4533a
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-10-10 19:34:41 +1000

    chore: some cleanups

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -22,17 +22,12 @@ const doc = new GoogleSpreadsheet(SPREADSHEET_ID as string, serviceAccountAuth);
 
 // Append Function
 export const saveToSpreadsheet = async (row: string[] | Record<string, string>) => {
-  console.log('saveToSpreadsheet')
   try {
     // loads document properties and worksheets
     await doc.loadInfo();
-    console.log(doc.title);
 
     const sheet = doc.sheetsByIndex[SHEET_ID];
-    console.log(sheet.title);
-    console.log(sheet.rowCount);
     const r = await sheet.addRow(row);
-    console.log('result', r)
   } catch (e) {
     console.error('Error: ', e);
   }
