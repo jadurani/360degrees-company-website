847ee3098c0f4afa7352b15f6f5fc7a8e2f48585
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-11-02 19:45:48 +0800

    feat: modal component shows up in the landing page

--- src/app/@modal/(.)projects/[slug]/page.tsx
+++ src/app/@modal/(.)projects/[slug]/page.tsx
@@ -4,14 +4,13 @@ import {
 } from "@components/StoryViewer/StoryViewer";
 import { getProjectFromSlug } from "../../../projects/completed-projects.constant";
 import { getImageWithBlurPlaceholder } from "@lib/generateBlurPlaceholder";
+import Modal from "@components/Modal/Modal";
 
 interface PhotoPageProps {
   params: { slug: string };
 }
 
 export default async function PhotoModal({ params: { slug } }: PhotoPageProps) {
-  // return <div>{slug}</div>;
-
   let projectDetails: StoryViewerProps | undefined = getProjectFromSlug(
     slug
   ) as StoryViewerProps;
@@ -31,17 +30,8 @@ export default async function PhotoModal({ params: { slug } }: PhotoPageProps) {
   }));
 
   return (
-    <div className="fixed z-[100] h-screen w-screen top-0 left-0">
-      <div className="relative">
-        {/* backdrop */}
-        <div className="absolute top-0 h-screen w-screen bg-neutral-900 lg:bg-neutral-900/90"></div>
-        {/* content */}
-        <div className="flex items-center justify-center w-screen h-screen">
-          <div className="w-screen h-screen lg:w-[768px] lg:h-[520px]">
-            <StoryViewer {...projectDetails}></StoryViewer>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Modal>
+      <StoryViewer {...projectDetails}></StoryViewer>
+    </Modal>
   );
 }
